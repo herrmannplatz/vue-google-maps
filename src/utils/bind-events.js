4184bdd1d3d09283196f0c787f40a261cdcc3b0e
herrmannplatz <rene@herrmannplatz.de>
2020-10-22 22:09:44 +0200

    chore: comment  issue

--- src/utils/bind-events.js
+++ src/utils/bind-events.js
@@ -1,5 +1,6 @@
 export default (vueInst, googleMapsInst, events) => {
   for (const eventName of events) {
+    // TODO: $listeners seemed to be removed in vue3
     if (vueInst.$gmapOptions.autobindAllEvents ||
         vueInst.$listeners[eventName]) {
       googleMapsInst.addListener(eventName, (ev) => {
